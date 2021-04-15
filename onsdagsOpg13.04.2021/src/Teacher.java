73ddc27c43b403931b61a221d7a0a19b14c1a815
Jens-k-m-m <jensmonteiro@gmail.com>
2021-04-15 12:01:07 +0200

    Alexander modtage Opdating fra siste ny  metedor  løser Opgave med cood Lab kl.11:47 dato 15-04-2021

--- onsdagsOpg13.04.2021/src/Teacher.java
+++ onsdagsOpg13.04.2021/src/Teacher.java
@@ -11,8 +11,9 @@ public class Teacher {
     for (int i = 0; i < arrayGrade.length; i++) {
 
       System.out.println("Enter grades: ");
-
-      arrayGrade[i] = validateGrade(scanner.nextInt());
+      int c = scanner.nextInt();
+      if (validateGrade(c) > 0)
+      arrayGrade[i] = c;
     }
   }
 
@@ -21,12 +22,11 @@ public class Teacher {
     if (number < -3 || number == -2 || number == -1 || number == 1 || number == 3 || number == 5
         || number == 6 || number == 8 || number == 9 || number == 11 || number > 12) {
 
-      //throw new IllegalArgumentException("fejl");
       System.out.println("fejl");
-
+      return -1;
 
     }
 
-    return number;
+    return 1;
   }
 }
